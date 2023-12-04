51393d65f935b031ad398e4a8e88bdfe2b9b7f58
anandukch <anandukch@gmail.com>
2023-12-05 01:55:34 +0530

    added multer

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
 const routes = require('express').Router();
+const multer = require("multer")
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+const upload = multer({ storage: storage });
+
 
 module.exports = routes;
